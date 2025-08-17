bc267fce73dc664f57f9c6f46bb1fac18dbc56fe
KIIZA TREVOUR <kiizatrevour33@gmail.com>
2025-08-17 10:09:35 +0300

    added task 2

--- MySQLServer.py
+++ MySQLServer.py
@@ -4,11 +4,10 @@ def create_database():
     try:
         cnx = mysql.connector.connect(user='root', password='@Trevour256!',
                                       host='localhost',port ='3308')
-        print("Connection established successfully.")
         cursor = cnx.cursor()
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         cursor.execute("USE alx_book_store")
-        print("Database created successfully.")
+        print("Database 'alx_book_store' created successfully!")
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Something is wrong with your user name or password")
